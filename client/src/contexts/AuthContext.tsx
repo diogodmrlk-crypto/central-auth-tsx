a61acb5e2ea34f649cad9db7b854563acd14d310
diogodmrlk-crypto <diogodmrlk@gmail.com>
2026-03-26 21:52:56 -0300

    Update AuthContext.tsx

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { KeyData, keysDatabase } from '@/data/keys';
+import { KeyData, keysDatabase } from '../data/keys';
 
 interface SessionData {
   activeKey: string;
